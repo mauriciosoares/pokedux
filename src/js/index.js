2d84cdef1e598834fb568f65290209948f6571fc
Mauricio Soares <m.soares2603@gmail.com>
2015-12-11 16:29:24 -0200

    add react router

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,41 @@
 import { render } from 'react-dom';
 import React, { Component } from 'react';
+import { Router, Route, Link } from 'react-router';
 import '../sass/style.sass';
+console.log('teste23');
 
 export default class App extends Component {
   render() {
     return (
-      <h1>hue2</h1>
+      <div>
+        <h1>test2</h1>
+
+        <Link to="/about">About</Link>
+        <Link to="/hue">Hue</Link>
+
+        {this.props.children}
+      </div>
     );
   }
 }
 
+function About() {
+  return (
+    <div>About</div>
+  );
+}
 
-render(<App />, document.getElementById('root'));
+function Hue() {
+  return (
+    <div>Hue</div>
+  );
+}
+
+render(
+  <Router>
+    <Route path="/" component={App}>
+      <Route path="about" component={About} />
+      <Route path="hue" component={Hue} />
+    </Route>
+  </Router>
+, document.getElementById('root'));
