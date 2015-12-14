78092ed801c5ad359dc73ea884df1abc798d4457
Mauricio Soares <m.soares2603@gmail.com>
2015-12-14 13:50:31 -0200

    add api url to constatns

--- src/js/utils/constants.js
+++ src/js/utils/constants.js
@@ -1,3 +1,6 @@
+export const API_URL = 'http://pokeapi.co/api/v1/pokedex/1/';
+export const POKEMON_URL = 'http://pokeapi.co/api/v1/pokemon/'; // + '1'
+
 export const pokemonsActions = {
   RECEIVE: 'RECEIVE'
 };
